0597f9126fd9a083eae572b41b1deb4ffb506ad8
Sydney <sydney@sydneys-mbp.lan>
2024-08-10 13:12:36 -0400

    updating files

---
+++ music_player copy/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <div className='header'>About</div>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel rutrum lorem. Proin non bibendum dolor, sit amet bibendum risus. Nam condimentum felis eu justo luctus varius. Nunc egestas congue neque in iaculis. Suspendisse potenti. Nunc consectetur risus non ligula sodales imperdiet. Sed a gravida orci. Nam eget est blandit, bibendum augue ut, blandit ante. Cras suscipit sem a erat commodo, eu ornare purus mattis. Morbi varius arcu sit amet tellus porta efficitur. Ut velit lectus, porttitor ac cursus sit amet, congue in magna. Nunc nunc nisi, tristique sed tellus et, tempor euismod nunc.
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- music_player/src/Main.js
+++ music_player/src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MusicPlayercopy from './MusicPlayercopy';
 import './App.css';
 
 const Main = () => {
   return (
     <>
     <div className="header_welcome">Welcome back</div><br />
+    <MusicPlayercopy />
     </>
   );
 }
